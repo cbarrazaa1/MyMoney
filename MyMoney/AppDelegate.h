27cb092fe410fb0b4a7265852b5e504d32c3bcc7
César Francisco Barraza <cbarrazaa1@fb.com>
2018-07-14 18:47:41 -0700

    Initial Commit

---
+++ MyMoney/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMoney
+//
+//  Created by César Francisco Barraza on 7/14/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyMoney/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyMoney
+//
+//  Created by César Francisco Barraza on 7/14/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
